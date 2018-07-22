4a7a12d0959906c1ac3287515f5228030cc73040
Justin Wang <justinw827@aol.com>
2018-07-22 20:53:39 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    int i = 0
+    newArray = []
+    while i < array.length
+      newElement = yield (array[1], block)
+      newArray.push(newElement)
+      i += 1
+    end
+    return newArray
+  else
+    puts "No block given."
+  end
+end
